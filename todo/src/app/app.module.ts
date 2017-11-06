5f7f981375065fb4e12255f4734373f7e32d453b
Tuan Anh Le <anhlt1983@gmail.com>
2017-11-06 12:46:11 +0900

    simple component demo

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -18,3 +18,8 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// this define root module by default
+// it tell Angular which feature that this app requires to run
+// it provide configuration by using @NgModule decorator
+// it tell Angular about components in the app, which one will be used to start application
--- todo/src/main.ts
+++ todo/src/main.ts
@@ -8,4 +8,11 @@ if (environment.production) {
   enableProdMode();
 }
 
+// calling bootstrap process for web-based application
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+// this is bootstrap file: main.ts
+// it fire Angular, then in turn, process html file, process todo-element
+// since selector define component match with todo-element, it replace
+// placeholder template.
+// place holder template is parsed, and {{}} binding is discovered and evaluated
